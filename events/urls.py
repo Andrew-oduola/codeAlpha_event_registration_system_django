46df1c68ac8cd53a66645c795a914d42c306263c
Drew <ayobamioduola13@gmail.com>
2025-03-05 05:26:58 +0100

    modified: added comments to the urls.py

--- events/urls.py
+++ events/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
-
 from .views import EventViewSet, EventRegistrationViewSet
 
-
+# Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
 router.register(r'registrations', EventRegistrationViewSet)
 
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
 ]
